65d3cd49871d539eb2b07c44230fafe51da05f48
Elle Sullivan <esullivan@duckduckgo.com>
2023-04-03 19:48:08 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DesignResourcesKit
+
+A description of this package.
---
+++ Sources/DesignResourcesKit/DesignResourcesKit.swift
@@ -0,0 +1,6 @@
+public struct DesignResourcesKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
