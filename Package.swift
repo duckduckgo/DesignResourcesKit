6d31f23172afbf9aeba35c39f5fdd8993c656959
Mariusz Śpiewak <mspiewak@duckduckgo.com>
2025-02-28 12:41:33 +0100

    Update min macOS platform version

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "DesignResourcesKit",
     platforms: [
         .iOS("15.0"),
-        .macOS("10.15")
+        .macOS("11.4")
     ],
     products: [
         .library(
