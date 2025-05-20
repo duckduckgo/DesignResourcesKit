caab4ed58bb50864a04f72422a4e5bcf869b3419
Chris Brind <brindy@duckduckgo.com>
2025-05-20 15:51:01 +0100

    add check solid

--- Sources/DesignResourcesKit/DesignSystemImages+Glyphs.swift
+++ Sources/DesignResourcesKit/DesignSystemImages+Glyphs.swift
@@ -320,6 +320,7 @@ public extension DesignSystemImages {
             public static var card: DesignSystemImage { .init(resource: .card24) }
             public static var checkRecolorable: DesignSystemImage { .init(resource: .checkRecolorable24) }
             public static var checkCircle: DesignSystemImage { .init(resource: .checkCircle24) }
+            public static var checkSolid: DesignSystemImage { .init(resource: .checkSolid24) }
             public static var chevronCircleDown: DesignSystemImage { .init(resource: .chevronCircleDown24) }
             public static var chevronDown: DesignSystemImage { .init(resource: .chevronDown24) }
             public static var chevronDownSmall: DesignSystemImage { .init(resource: .chevronDown24Small) }
